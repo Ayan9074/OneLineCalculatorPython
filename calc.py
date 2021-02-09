5c9aba7833fb91a68be5f9a1107a5878069bb998
Ayan9074 <57524544+Ayan9074@users.noreply.github.com>
2021-02-09 17:55:51 +0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,1 @@
-print(list(map(lambda x: print(eval(input('Enter the question(e.g.4+4): '))), ['']*int(input('How many Operations would you like to complete? ')))))
+print([*map(lambda x: print(eval(input('Enter the question(e.g.4+4): '))), ['']*int(input('How many Operations would you like to complete? ')))])
